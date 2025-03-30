cca07fb71a54f33e3412a7ad4180c9caef7d0e64
AKASH-KR-2 <akashkrkr282@gmail.com>
2025-03-30 10:38:08 +0530

    test

---
+++ New folder/.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main ()  {
+
+
+        int a=50;
+        int b=5;
+        int c=0;
+
+
+        while (a<50)
+        {
+
+            a-=b;
+            c++;
+    
+
+        }
+
+        printf("time taken to ate bannna is : %d \t",c)
+
+
+
+}
--- arr.c
+++ arr.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
 int main() {
+    int a[4];
+
+    // value adding to array using index a[index] = value;
+    a[0] = 10;
+    a[1] = 20;
+    a[2] = 30;
+    a[3] = 40;
+
+    printf("[%d] ", a[0]);
+    printf("[%d] ", a[1]);
+    printf("[%d] ", a[2]);
+    printf("[%d]", a[3]);
+
+    printf("\nArray printing in loop => ");
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("[%d] ", a[i]);
+    }
+    
+
     return 0;
 }
